f3690e2f5e3fe18e916a370d5f538d9db0a2cc66
TahaAissat <tahayacineaissat@gmail.com>
2023-09-19 18:52:14 +0200

    front 19h

--- home.js
+++ home.js
@@ -2,40 +2,35 @@ document.querySelector('#btn-search').addEventListener('click', function () {
 const departure = document.querySelector('#departure');
 const arrival = document.querySelector('#arrival');
 const date = document.querySelector('#date');
+console.log(date.value)
 
-fetch(`https://tickethack-backend-ochre.vercel.app/home/search/${departure}/${arrival}/${date}`)
+fetch(`http://localhost:3000/home/search/${departure.value}/${arrival.value}/${date.value}`)
 .then(response=>response.json())
 .then(trips => {
-    if(!trips.result){
+    console.log(trips)
+   /* if(!trips.result){
         // Afficher loupe et message
         document.querySelector('#resultat').innerHTML = `
-
         <img src="images/notfound.png" id="notfound"/>
         <p id="textbase">No trip found.</p>`
-
     } else {
-        document.querySelector('#resultat').innerHTML = `
-        
+        document.querySelector('#resultat').innerHTML = ` 
         <div id="book">
-        <p id="trajets">`${trips.departure}` > `${trips.arrival}` `${trips.date}` `${trips.price}` € </p>
+        <p id="trajets">${trips.departure}>${trips.arrival}${trips.date}${trips.price}€ </p>
         <button type="button" id="btn-book">Book</button>
-        </div>
-
-        
+        </div>        
         `
-        // Afficher les résultats
-
-
-    }
-
-    
+        // Afficher les résultats*/
+    } )  
 })
+//}
+//)
 
 
-fetch(`https://tickethack-backend-ochre.vercel.app/home//addCart`)
+/*fetch(`https://tickethack-backend-ochre.vercel.app/home/addCart`)
 .then(response=>response.json())
 .then(data => {
-
+console.log(data)
     // Fetch la route qui ajoute dans le cart lorsqu'on appuie sur le bouton book 
 
-}):
+})*/
