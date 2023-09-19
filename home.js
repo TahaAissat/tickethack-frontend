6798d16870f50e97f9ae1d86e4fbcb7cab71bc59
TahaAissat <tahayacineaissat@gmail.com>
2023-09-19 17:22:41 +0200

    correction home.js

--- home.js
+++ home.js
@@ -3,7 +3,7 @@ const departure = document.querySelector('#departure');
 const arrival = document.querySelector('#arrival');
 const date = document.querySelector('#date');
 
-fetch(`https://tickethack-backend-ochre.vercel.app/search/${departure}/${arrival}/${date}`)
+fetch(`https://tickethack-backend-ochre.vercel.app/search/${departure.value}/${arrival.value}/${date.value}`)
 .then(response=>response.json())
 .then(trips => {
     if(!trips.result){
